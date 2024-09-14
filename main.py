98c01d6ffb036a05e984778e998a6ae8799b81ac
hameelhusnain <husnainhameel3@gmail.com>
2024-09-14 23:56:42 +0500

    added opencage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import phonenumbers
+import opencage
 from myphone import number
 
 from phonenumbers import geocoder
@@ -6,3 +7,10 @@ from phonenumbers import geocoder
 pepnumber = phonenumbers.prase(number)
 location = geocoder.description_for_number(pepnumber, "en")
 print(location)
+
+
+from phonenumbers import carrier
+service_pro = phonenumbers.parse(number)
+print(carrier.name_for_number(service_pro, "en"))
+
+from opencage.geocoder import OpenCageGeocode
