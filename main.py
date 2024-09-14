1cbb1e8a78b31c02c72f561349a2a617ad63bda9
hameelhusnain <husnainhameel3@gmail.com>
2024-09-15 00:27:09 +0500

    add changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import phonenumbers
-# import opencage
+import opencage
 from myphone import number
 
 from phonenumbers import geocoder
@@ -13,4 +13,10 @@ from phonenumbers import carrier
 service_pro = phonenumbers.parse(number)
 print(carrier.name_for_number(service_pro, "en"))
 
-# from opencage.geocoder import OpenCageGeocode
+from opencage.geocoder import OpenCageGeocode
+
+key = '58aeced208bc43d8bad96d1cf285c9ea'
+
+geocoder = OpenCageGeocode(key)
+query = str(location)
+result = geocoder
